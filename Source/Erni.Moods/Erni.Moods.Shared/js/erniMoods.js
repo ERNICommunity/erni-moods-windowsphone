6c759177066d8f38fa9652213c0db1c57d420fb3
duracellko <rasto@duracellko.net>
2014-10-23 21:30:58 +0800

    Show all users on map as pushpins.

---
+++ Source/Erni.Moods/Erni.Moods.Shared/js/erniMoods.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+
+(function () {
+    var erniMoodsBaseUrl = "http://moodyrest.azurewebsites.net/moods";
+
+    this.ErniMoods = this.ErniMoods || {};
+    var ns = this.ErniMoods;
+
+    ns.ErniMoodsClient = function () { }
+
+    ns.ErniMoodsClient.prototype = {
+        baseUrl: function () { return erniMoodsBaseUrl; },
+        getMoodsAsync: function () {
+            return this._sendHttpRequest(this.baseUrl());
+        },
+        _sendHttpRequest: function (url, data) {
+            var uri = new Windows.Foundation.Uri(url);
+            var client = new Windows.Web.Http.HttpClient();
+            
+            var requestPromise = null;
+            if (typeof (data) != "undefined" && data != null) {
+                var content = "";
+                if (typeof (data) == "String") {
+                    content = data;
+                }
+                else {
+                    content = JSON.stringify(data);
+                }
+                requestPromise = client.postAsync(uri, new Windows.Web.Http.HttpStringContent(content));
+            }
+            else {
+                requestPromise = client.getAsync(uri);
+            }
+
+            return requestPromise.then(function (response) {
+                return response.content.readAsStringAsync()
+            }).then(function (data) {
+                return JSON.parse(data);
+            });
+        }
+    }
+
+})();
--- Source/Erni.Moods/Erni.Moods.Shared/pages/mapWithMoods.js
+++ Source/Erni.Moods/Erni.Moods.Shared/pages/mapWithMoods.js
@@ -8,6 +8,9 @@
     var session = WinJS.Application.sessionState;
     var util = WinJS.Utilities;
     WinJS.UI.Pages.define("/pages/mapWithMoods.html", {
+        _map: null,
+        _moodsClient: new ErniMoods.ErniMoodsClient(),
+
         processed: function (element) {
             return WinJS.Resources.processAll(element);
         },
@@ -15,24 +18,45 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: initMap, culture: "en-us", homeRegion: "US" });
-            function initMap() {
-                var map;
+            var t = this;
 
-                var mapOptions =
-                {
-                    credentials: "AkUeZZ_8CT2Y1VawMWhAIPyfKwEtKLxtrj0TkHVMAR4J8TCCdcJ6z6-rvk14mMFP",
-                    center: new Microsoft.Maps.Location(49.029645, 21.276521),
-                    zoom: 10,
-                    mapTypeId: Microsoft.Maps.MapTypeId.aerial
-                };
-
-                map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
-            }
+            Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: function () { t._initMapCallback(t); }, culture: "en-us", homeRegion: "US" });
         },
 
         unload: function () {
             // TODO: Respond to navigations away from this page.
         },
+        _initMapCallback: function (t) {
+            var mapOptions =
+            {
+                credentials: "AkUeZZ_8CT2Y1VawMWhAIPyfKwEtKLxtrj0TkHVMAR4J8TCCdcJ6z6-rvk14mMFP",
+                center: new Microsoft.Maps.Location(49.029645, 21.276521),
+                zoom: 10,
+                mapTypeId: Microsoft.Maps.MapTypeId.aerial
+            };
+
+            t._map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+
+            t._moodsClient.getMoodsAsync().done(
+                function (data) {
+                    if (data.length > 0) {
+                        // focus on first user
+                        var user = data[0];
+                        var location = new Microsoft.Maps.Location(user.location[0], user.location[1]);
+                        t._map.setView({ center: location });
+
+                        // show pushpins
+                        for (var i in data) {
+                            var user = data[i];
+                            var location = new Microsoft.Maps.Location(user.location[0], user.location[1]);
+                            var pushpin = new Microsoft.Maps.Pushpin(location);
+                            t._map.entities.push(pushpin);
+                        }
+                    }
+                },
+                function () {
+                    alert("Mood posting failed.");
+                });
+        }
     });
 })();
