f33f9a5a087c93c06db55d8c9f5a08464b959f76
duracellko <rasto@duracellko.net>
2014-10-23 19:58:40 +0800

    Display Bing map after selecting mood.

--- Source/Erni.Moods/Erni.Moods.Shared/js/moodsList.js
+++ Source/Erni.Moods/Erni.Moods.Shared/js/moodsList.js
@@ -1,52 +1,52 @@
 ﻿(function () {
 
-    WinJS.UI.Pages.define("/pages/default.html", {
-        // This function is called whenever a user navigates to this page. It
-        // populates the page elements with the app's data.
-        ready: function (element, options) {
-            // TODO: Initialize the page here.
+    ////WinJS.UI.Pages.define("/pages/default.html", {
+    ////    // This function is called whenever a user navigates to this page. It
+    ////    // populates the page elements with the app's data.
+    ////    ready: function (element, options) {
+    ////        // TODO: Initialize the page here.
 
-            // When the page loads, check the view state and
-            // use the appropriate layout.
+    ////        // When the page loads, check the view state and
+    ////        // use the appropriate layout.
 
-            this.updateLayout(element, Windows.UI.ViewManagement.ApplicationView, null);
+    ////        this.updateLayout(element, Windows.UI.ViewManagement.ApplicationView, null);
 
-        },
+    ////    },
 
-        unload: function () {
-            // TODO: Respond to navigations away from this page.
-        },
+    ////    unload: function () {
+    ////        // TODO: Respond to navigations away from this page.
+    ////    },
 
-        updateLayout: function (element, viewState, lastViewState) {
+    ////    updateLayout: function (element, viewState, lastViewState) {
 
-            // Respond to changes in viewState.
+    ////        // Respond to changes in viewState.
 
-            // Get the ListView control. 
-            var viewStateExampleListView =
-                element.querySelector("#viewStateExampleListView").winControl;
+    ////        // Get the ListView control. 
+    ////        var viewStateExampleListView =
+    ////            element.querySelector("#viewStateExampleListView").winControl;
 
-            // Use a ListLayout if the app is in portrait mode. 
-            if (viewState.getForCurrentView().orientation) {
+    ////        // Use a ListLayout if the app is in portrait mode. 
+    ////        if (viewState.getForCurrentView().orientation) {
 
-                // If layout.orientation is horizontal, the ListView
-                // is already using a ListLayout, so we don't
-                // have to do anything. We only need to switch
-                // layouts when layout.orientation is horizontal. 
-                if (viewStateExampleListView.layout.orientation == "horizontal") {
-                    viewStateExampleListView.layout = new WinJS.UI.ListLayout();
-                }
-            }
+    ////            // If layout.orientation is horizontal, the ListView
+    ////            // is already using a ListLayout, so we don't
+    ////            // have to do anything. We only need to switch
+    ////            // layouts when layout.orientation is horizontal. 
+    ////            if (viewStateExampleListView.layout.orientation == "horizontal") {
+    ////                viewStateExampleListView.layout = new WinJS.UI.ListLayout();
+    ////            }
+    ////        }
 
-                // Use a GridLayout if the app isn't in portrait mode. 
-            else {
-                // Only switch layouts if layout.orientation is vertical. 
-                if (viewStateExampleListView.layout.orientation == "vertical") {
-                    viewStateExampleListView.layout = new WinJS.UI.GridLayout();
-                }
-            }
+    ////            // Use a GridLayout if the app isn't in portrait mode. 
+    ////        else {
+    ////            // Only switch layouts if layout.orientation is vertical. 
+    ////            if (viewStateExampleListView.layout.orientation == "vertical") {
+    ////                viewStateExampleListView.layout = new WinJS.UI.GridLayout();
+    ////            }
+    ////        }
 
-        }
-    });
+    ////    }
+    ////});
 
     "use strict";
     var dataArray = [
---
+++ Source/Erni.Moods/Erni.Moods.Shared/pages/mapWithMoods.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="/Scripts/jquery-2.1.1.js" />
+/// <reference path="/Bing.Maps.JavaScript/js/veapicore.js" />
+
+(function () {
+    "use strict";
+
+    var nav = WinJS.Navigation;
+    var session = WinJS.Application.sessionState;
+    var util = WinJS.Utilities;
+    WinJS.UI.Pages.define("/pages/mapWithMoods.html", {
+        processed: function (element) {
+            return WinJS.Resources.processAll(element);
+        },
+
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: initMap, culture: "en-us", homeRegion: "US" });
+            function initMap() {
+                var map;
+
+                var mapOptions =
+                {
+                    credentials: "AkUeZZ_8CT2Y1VawMWhAIPyfKwEtKLxtrj0TkHVMAR4J8TCCdcJ6z6-rvk14mMFP",
+                    center: new Microsoft.Maps.Location(49.029645, 21.276521),
+                    zoom: 10,
+                    mapTypeId: Microsoft.Maps.MapTypeId.aerial
+                };
+
+                map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+            }
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+    });
+})();
